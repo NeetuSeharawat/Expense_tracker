888ba149d0dce10ac87150e36e1e3c440361aaea
Neetu Seharawat <neetumorbca@gmail.com>
2023-03-22 19:28:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SignUpPage from './Components/SignUpPage/SignUpPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    < SignUpPage />
+        </div>
+  );
+}
+
+export default App;
