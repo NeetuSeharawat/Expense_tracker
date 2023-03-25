5f679cb32fa73f24c6bea18fd0a61228a0d177c3
Neetu Seharawat <neetumorbca@gmail.com>
2023-03-25 17:47:33 +0530

    Subscribing and Dispatching Actions - (using Redux Data in React)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-
-import SignUpPage from './Components/SignUpPage/SignUpPage';
-import LoginPage from './Components/LoginPage/LoginPage';
-import IncompleteProfile from './Components/LoginPage/IncompleteProfile';
-import UpdateProfile from './Components/LoginPage/UpdateProfile';
-import { Fragment } from 'react';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
-    <Fragment>
-
-    <SignUpPage />
-    <LoginPage/>
-    <IncompleteProfile />
-    <UpdateProfile />
-
-    </Fragment>
+    <Counter />
   );
 }
 
