87044819d239067f2dc976e24530a540a666f911
Neetu Seharawat <neetumorbca@gmail.com>
2023-03-26 11:53:59 +0530

    Decrease by 5 and ToggleCounter Button implemented successfully (using Redux data in React)

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -5,7 +5,12 @@ const Counter = () => {
   const counter = useSelector(state=>state.counter);
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  const show = useSelector(state=>state.showCounter);
+// we can use useSelector multiple times to retrive the data.
+
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  }
 //Increment by1
   const increment = () => {
     dispatch({type:'increment'});
@@ -19,19 +24,19 @@ const Counter = () => {
     dispatch({type:'decrement'});
   }
   //decrement by 5
-  // const decrementHandler = () => {
-  //   dispatch({type:'Decrease by 5', action :5});
-  // }
+  const decrementHandler = () => {
+    dispatch({type:'Decrease by 5', amount :5});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+    {show && <div className={classes.value}>{counter}</div>}
       <button onClick={increment}>Increment</button>
       <button onClick={incrementHandler}>Increase by 5</button>
 <hr />
       <button onClick={decrement}>Decrement</button>
-      {/* <button onClick={decrementHandler}>Decrease by 5</button> */}
+      <button onClick={decrementHandler}>Decrease by 5</button>
 <hr />
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,45 @@
 import {createStore} from 'redux';
 
-const CounterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter:true };
+
+const CounterReducer = (state =initialState , action) => {
 
       if(action.type === 'increment'){
-        return { counter: state.counter + 1,
+        return { 
+          counter: state.counter + 1,
+          showCounter : state.showCounter
         };
       }
     
       if(action.type === 'Increase by 5'){
-        return { counter: state.counter+action.amount,
+        return {
+          counter: state.counter+action.amount,
+          showCounter : state.showCounter
+
         };
       }
 
       if(action.type === 'decrement'){
-        return { counter: state.counter - 1,
+        return { 
+        counter: state.counter - 1,
+        showCounter : state.showCounter
+
         };
       }
 
-      // if(action.type === 'Decrease by 5'){
-      //   return { counter: state.counter-action.amount,
-      //   };
-      // }
+      if(action.type === 'Decrease by 5'){
+        return { counter: state.counter-action.amount,
+        showCounter : state.showCounter
+
+        };
+      }
+
+      if(action.type === 'toggle'){
+      return{
+        showCounter : !state.showCounter,
+       counter : state.counter
+      }
+      }
 
     return state;
   };
